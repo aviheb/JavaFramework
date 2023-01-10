be1796eee4c1d3c3457e64101d1964f3f8ad1a53
aviheb <121996064+aviheb@users.noreply.github.com>
2023-01-10 23:58:05 +0200

    Add files via upload

---
+++ Spring-Boot-Framework-Tutorials/application/Spring-Boot-Framework-Tutorials/src/main/java/com/app/SpringBootFrameworkTutorials/SpringBootFrameworkTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.app.SpringBootFrameworkTutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFrameworkTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFrameworkTutorialsApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-Framework-Tutorials/application/Spring-Boot-Framework-Tutorials/src/test/java/com/app/SpringBootFrameworkTutorials/SpringBootFrameworkTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.SpringBootFrameworkTutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFrameworkTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
